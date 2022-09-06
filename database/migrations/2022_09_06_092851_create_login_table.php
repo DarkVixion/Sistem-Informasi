bd581ef5ea619bcf89d5a0ee2d628a1af3932ddc
diaaand <diaaanaaad@gmail.com>
2022-09-06 16:38:38 +0700

    update table login

---
+++ database/migrations/2022_09_06_092851_create_login_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoginTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('login', function (Blueprint $table) {
+            $table->id();
+            $table->double("email");
+            $table->double("password");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login');
+    }
+}
