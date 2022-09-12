21d97baee5c02af0b998c6156e5a73ee1db0bae6
Aran27 <ashabrivanyayardan@gmail.com>
2022-09-12 09:38:14 +0700

    Update Controller

--- app/Http/Controllers/JenisMitraController.php
+++ app/Http/Controllers/JenisMitraController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class JenisMitraController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('JenisMitra');
+    }
 }
--- app/Http/Controllers/TambahKerjasamaController.php
+++ app/Http/Controllers/TambahKerjasamaController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class TambahKerjasamaController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('Kerjasama');
+    }
+
+    public function tambah()
+    {
+        return view('TambahKerja');
+    }
 }
