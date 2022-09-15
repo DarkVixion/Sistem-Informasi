d45011819be2e23599ceabf50fedea583e9a2e99
DarkVixion <hafizhfuq12@gmail,com>
2022-09-15 16:05:47 +0700

    Fix Href

--- resources/views/SkalaKerja.blade.php
+++ resources/views/SkalaKerja.blade.php
@@ -183,7 +183,7 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link">
+                                    <a href="/JenisMitra" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Jenis Mitra</p>
                                     </a>
