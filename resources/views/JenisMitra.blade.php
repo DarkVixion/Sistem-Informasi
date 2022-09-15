3d3ff9cc4fe6cfb873bdf321b55c6154f3bddfee
berlianii14 <b.istadewi@gmail.com>
2022-09-15 14:09:44 +0700

    update page jenis mitra skala kerja

--- resources/views/JenisMitra.blade.php
+++ resources/views/JenisMitra.blade.php
@@ -214,6 +214,37 @@
         <!-- /.sidebar -->
     </aside>
 
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Jenis Mitra</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Master Data</a></li>
+                            <li class="breadcrumb-item active">Jenis Mitra</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+
+                    
+        </section>
+    </div>                      
+    
+            <!-- /.card -->
+
+
+
+
+
+
     <!-- jQuery -->
     <script src="plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
