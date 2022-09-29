9e29e977df09919e8a7c28ed93d9fb4ca1318425
diaaand <diaaanaaad@gmail.com>
2022-09-29 16:22:31 +0700

    Update view akun

--- resources/views/Akun.blade.php
+++ resources/views/Akun.blade.php
@@ -50,7 +50,7 @@
     <!-- /.container-fluid -->
 
     <section class="content">
-        <form class="form-horizontal" action="{{route('inputdataakun')}}"  method="POST" enctype="multipart/form-data">
+        <form class="form-horizontal" action="{{route('inputdataakun')}}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="container-fluid">
                 <div class="row">
@@ -59,9 +59,9 @@
                             <!-- /.card-header -->
                             <div class="card-body table-responsive p-3">
                                 <div class="d-flex justify-content-center">
-                                    <img class=" profile-user-img img-fluid img-circle" {{-- src="{{ asset('profilpic/'.$akun->path_profileakun) }}" --}} alt=" User profile picture">
+                                    <img class=" profile-user-img img-fluid img-circle" src="{{ asset('profilpic/'.$akun->path_profileakun) }}" alt=" User profile picture">
                                 </div>
-                                <h3 class="profile-username text-center">Admin UPer</h3>
+                                <h3 class="profile-username text-center">{{ $akun->namaakun }}</h3>
                                 <p class="text-muted text-center">Universitas Pertamina</p><br><br>
                                 <div class="form-group row">
                                     <label for="path_mou" class="col-sm-2 col-form-label ">Foto Profile</label>
@@ -72,38 +72,38 @@
                                 <div class="form-group row">
                                     <label for="input" class="col-sm-2 col-form-label">Nama Pegawai</label>
                                     <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="namaakun" placeholder="Admin UPer" {{-- value="{{ $akun->namaakun }}" --}}>
+                                        <input type="text" class="form-control" name="namaakun" placeholder="Admin UPer" value="{{ $akun->namaakun }}">
                                     </div>
                                 </div><br>
                                 <div class=" form-group row">
                                     <label for="input" class="col-sm-2 col-form-label">Username SSO</label>
                                     <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="userssoakun" placeholder="admin_UPer01" {{-- value="{{ $akun->userssoakun }}" --}}>
+                                        <input type="text" class="form-control" name="userssoakun" placeholder="admin_UPer01" value="{{ $akun->userssoakun }}">
                                     </div>
                                 </div><br>
                                 <div class="form-group row">
                                     <label for="input" class="col-sm-2 col-form-label">E-Mail</label>
                                     <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="emailakun" placeholder="admin.uper@dududu.ac.id" {{-- value="{{ $akun->emailakun }}" --}}>
+                                        <input type="text" class="form-control" name="emailakun" placeholder="admin.uper@dududu.ac.id" value="{{ $akun->emailakun }}">
                                     </div>
                                 </div><br>
                                 <div class="form-group row">
                                     <label for="input" class="col-sm-2 col-form-label">NIP</label>
                                     <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="nipakun" placeholder="122333" {{-- value="{{ $akun->nipakun }}" --}}>
+                                        <input type="text" class="form-control" name="nipakun" placeholder="122333" value="{{ $akun->nipakun }}">
                                     </div>
                                 </div></br>
                                 <div class="form-group row">
                                     <label for="input" class="col-sm-2 col-form-label">No Telepon</label>
                                     <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="notelpakun" placeholder="0812xxx" {{-- value="{{ $akun->notelpakun }}" --}}>
+                                        <input type="text" class="form-control" name="notelpakun" placeholder="0812xxx" value="{{ $akun->notelpakun }}">
                                     </div>
                                 </div><br>
                                 <div class="form-group row">
                                     <label for="select" class="col-sm-2 col-form-label">Role</label>
                                     <div class="col-sm-10">
                                         <div class="form-group">
-                                            <select class="form-control" name="roleakun" {{-- value="{{ $akun->roleakun }}" --}}>
+                                            <select class="form-control" name="roleakun" value="{{ $akun->roleakun }}">
                                                 <option {{-- if ($akun->roleakun == 'Role 1') selected @else "" @endif --}}> Role 1</option>
                                                 <option {{-- if ($akun->roleakun == 'Role 2') selected @else "" @endif --}}> Role 2</option>
                                                 <option {{-- if ($akun->roleakun == 'Role 3') selected @else "" @endif --}}> Role 3</option>
@@ -117,7 +117,7 @@
                                     <label for="select" class="col-sm-2 col-form-label">Status</label>
                                     <div class="col-sm-10">
                                         <div class="form-group">
-                                            <select class="form-control" name="statusakun" {{-- value="{{ $akun->statusakun }}" --}}>
+                                            <select class="form-control" name="statusakun" value="{{ $akun->statusakun }}">
                                                 <option {{-- if ($akun->statusakun == 'Aktif') selected @else "" @endif --}}>Aktif</option>
                                                 <option {{-- if ($akun->statusakun == 'Tidak Aktif') selected @else "" @endif --}}>Tidak Aktif</option>
                                             </select>
