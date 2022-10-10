6d0d630e826abb1bf3f71e0c9b227519cd3d0122
berlianii14 <b.istadewi@gmail.com>
2022-10-10 15:29:01 +0700

    rapihin ukuran

--- resources/views/TambahKerja.blade.php
+++ resources/views/TambahKerja.blade.php
@@ -289,30 +289,30 @@
                                         <div class="col-sm-10">
                                         </div>
                                         <br>
-                                        <label for="judul_mou" class="col-sm-2 col-form-label">Judul Kerja Sama</label>
-                                        <div class="col-sm-10">
+                                        <label for="judul_mou" class="col-sm-3 col-form-label">Judul Kerja Sama</label>
+                                        <div class="col-sm-9">
                                             <input type="text"
                                                 class="form-control @error('judul_mou') is-invalid @enderror"
                                                 name="judul_mou" placeholder="Masukan Judul Kerja Sama">
                                         </div>
                                         <br><br>
-                                        <label for="tglmulai_mou" class="col-sm-2 col-form-label">Tanggal Mulai</label>
-                                        <div class="col-sm-10">
+                                        <label for="tglmulai_mou" class="col-sm-3 col-form-label">Tanggal Mulai</label>
+                                        <div class="col-sm-9">
                                             <input type="date"
                                                 class="form-control @error('tglmulai_mou') is-invalid @enderror"
                                                 name="tglmulai_mou">
                                         </div>
                                         <br><br>
-                                        <label for="tglselesai_mou" class=" col-sm-2 col-form-label ">Tanggal
+                                        <label for="tglselesai_mou" class=" col-sm-3 col-form-label ">Tanggal
                                             Selesai</label>
-                                        <div class=" col-sm-10 ">
+                                        <div class=" col-sm-9 ">
                                             <input type="date"
                                                 class="form-control @error('tglselesai_mou') is-invalid @enderror"
                                                 name="tglselesai_mou">
                                         </div>
                                         <br><br>
-                                        <label for="path_mou" class="col-sm-2 col-form-label ">Dokumen MoU</label>
-                                        <div class="col-sm-10 ">
+                                        <label for="path_mou" class="col-sm-3 col-form-label ">Dokumen MoU</label>
+                                        <div class="col-sm-9 ">
                                             <input type="file" class="form-control " name="path_mou[]" accept="pdf/*"
                                                 multiple>
                                         </div><br><br>
@@ -328,7 +328,7 @@
                                     </div>
                                 </div>
                                 <div class=" card-footer ">
-                                    <button type="" class="btn btn-info float-right">Tambah MOU</button>
+                                    <button type="button" class="btn btn-info float-right">Tambah MOU</button>
                                 </div>
                             </div>
                         </div>
@@ -346,29 +346,29 @@
                                 <div class="card-body">
                                     <h3 style="text-align: center;">Memorandum of Aggreement (MoA)</h3>
                                     <div class="form-group row">
-                                        <label for="input" class="col-sm-2 col-form-label"> </label>
-                                        <div class="col-sm-10">
+                                        <label for="input" class="col-sm-3 col-form-label"> </label>
+                                        <div class="col-sm-9">
                                         </div>
                                         <br>
-                                        <label for="judul_moa" class="col-sm-2 col-form-label">Judul
+                                        <label for="judul_moa" class="col-sm-3 col-form-label">Judul
                                             Kerjasama</label>
-                                        <div class="col-sm-10">
+                                        <div class="col-sm-9">
                                             <input type="text"
                                                 class="form-control @error('judul_moa') is-invalid @enderror"
                                                 name="judul_moa" placeholder="Masukan Judul Kerja Sama">
                                         </div>
-                                        <br>
-                                        <label for="nilaikontrak" class="col-sm-2 col-form-label">Nilai
+                                        <br><br>
+                                        <label for="nilaikontrak" class="col-sm-3 col-form-label">Nilai
                                             Kontrak</label>
-                                        <div class="col-sm-10">
+                                        <div class="col-sm-9">
                                             <input type="number"
                                                 class="form-control @error('nilaikontrak') is-invalid @enderror"
                                                 name="nilaikontrak" placeholder="Masukan Nilai Kontrak (Rp)">
                                         </div>
-                                        <br>
-                                        <label for="select " class="col-sm-2 col-form-label ">Lingkup Kerja
+                                        <br><br>
+                                        <label for="select " class="col-sm-3 col-form-label ">Lingkup Kerja
                                             Sama</label>
-                                        <div class="col-sm-10 ">
+                                        <div class="col-sm-9 ">
                                             <select class="form-control" name="lingkupkerja">
                                                 <option value="" hidden>--- Pilih Lingkup Kerja ---</option>
                                                 @foreach ($lk as $item)
@@ -376,32 +376,32 @@
                                                 @endforeach
                                             </select>
                                         </div>
-                                        <br>
-                                        <label for="tglmulai_moa" class="col-sm-2 col-form-label">Tanggal
+                                        <br><br>
+                                        <label for="tglmulai_moa" class="col-sm-3 col-form-label">Tanggal
                                             Mulai</label>
-                                        <div class="col-sm-10">
+                                        <div class="col-sm-9">
                                             <input type="date"
                                                 class="form-control @error('tglmulai_moa') is-invalid @enderror"
                                                 name="tglmulai_moa">
                                         </div>
-                                        <br>
-                                        <label for="tglselesai_moa" class=" col-sm-2 col-form-label ">Tanggal
+                                        <br><br>
+                                        <label for="tglselesai_moa" class=" col-sm-3 col-form-label ">Tanggal
                                             Selesai</label>
-                                        <div class=" col-sm-10 ">
+                                        <div class=" col-sm-9 ">
                                             <input type="date"
                                                 class="form-control @error('tglselesai_moa') is-invalid @enderror"
                                                 name="tglselesai_moa">
                                         </div>
-                                        <br>
-                                        <label for="path_moa" class="col-sm-2 col-form-label ">Dokumen MoA</label>
-                                        <div class="col-sm-10 ">
+                                        <br><br>
+                                        <label for="path_moa" class="col-sm-3 col-form-label ">Dokumen MoA</label>
+                                        <div class="col-sm-9 ">
                                             <input type="file" class="form-control" name="path_moa[]" accept="pdf/*"
                                                 multiple>
                                         </div>
                                     </div>
                                 </div>
                                 <div class=" card-footer ">
-                                    <button type="" class="btn btn-info float-right">Tambah MOA</button>
+                                    <button type="button" class="btn btn-info float-right">Tambah MOA</button>
                                 </div>
                             </div>
                         </div>
@@ -414,20 +414,22 @@
                                 <!-- form start -->
                                 <div class="card-body">
                                     <div class="form-group row">
-                                        <label for="input" class="col-sm-2 col-form-label">Narahubung</label>
-                                        <div class="col-sm-10">
+                                        <label for="input" class="col-sm-3 col-form-label">Narahubung</label>
+                                        <div class="col-sm-9">
                                             <input type="text" class="form-control" name="narahubung"
                                                 placeholder="Masukkan Narahubung">
                                         </div>
-                                        <label for="inputPassword3" class="col-sm-2 col-form-label">Nomor
+                                        <br><br>
+                                        <label for="inputPassword3" class="col-sm-3 col-form-label">Nomor
                                             Telepon</label>
-                                        <div class="col-sm-10">
+                                        <div class="col-sm-9">
                                             <input type="number" class="form-control " name="notelpnara"
                                                 placeholder="No. Telepon" pattern="/^-?\d+\.?\d*$/"
                                                 onKeyPress="if(this.value.length==15) return false;">
                                         </div>
-                                        <label for="inputPassword3 " class="col-sm-2 col-form-label ">Email</label>
-                                        <div class="col-sm-10 ">
+                                        <br><br>
+                                        <label for="inputPassword3 " class="col-sm-3 col-form-label ">Email</label>
+                                        <div class="col-sm-9 ">
                                             <input type="text" class="form-control" name="emailnara"
                                                 placeholder="Alamat Email">
                                         </div>
