4d89a195f3ffa80e134ff75857d3016b973c4971
DarkVixion <hafizhfuq12@gmail,com>
2022-10-13 07:33:15 +0700

    Update Import excel

--- resources/views/JenisMitra.blade.php
+++ resources/views/JenisMitra.blade.php
@@ -27,8 +27,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <button type="button" class="btn btn-default" data-toggle="modal"
-                            data-target="#modal-xl"
+                        <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-xl"
                             style="float:right; background-color:lightblue; border-radius:15px;">
                             Tambah Jenis Mitra
                         </button>
@@ -49,11 +48,14 @@
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $item->juduljenismitra }}</td>
                                     <td>
-                                        <button class="btn btn-primary" data-target="#modal-xxl{{ $item->id }}" data-toggle="modal"><i class="fa fa-edit"></i></button>                                        
-                                        <form action="{{route('hapus_mitra', $item->id)}}" method="POST" style="display:inline ">
+                                        <button class="btn btn-primary" data-target="#modal-xxl{{ $item->id }}"
+                                            data-toggle="modal"><i class="fa fa-edit"></i></button>
+                                        <form action="{{route('hapus_mitra', $item->id)}}" method="POST"
+                                            style="display:inline ">
                                             {{ method_field('DELETE') }}
                                             {{ csrf_field() }}
-                                            <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                            <button type="submit" class="btn btn-danger"><i
+                                                    class="fa fa-trash"></i></button>
                                         </form>
                                     </td>
                                 </tr>
@@ -64,25 +66,32 @@
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <h4 class="modal-title">Edit Jenis Mitra</h4>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
                                             </div>
                                             <form action="{{route('edit_mitra', $item->id)}}" method="post">
-                                            {!! csrf_field() !!}
-                                            @method("PATCH")
-                                            <div class="modal-body">
-                                                <div class="form-group row ">
-                                                    <label for="inputPassword3 " class="col-sm-2 col-form-label ">Jenis Mitra</label>
-                                                    <div class="col-sm-10 ">
-                                                        <input type="text" name="juduljenismitra" id="juduljenismitra" class="form-control " placeholder="Masukan Jenis Mitra Baru" value="{{ $item->juduljenismitra }}">
+                                                {!! csrf_field() !!}
+                                                @method("PATCH")
+                                                <div class="modal-body">
+                                                    <div class="form-group row ">
+                                                        <label for="inputPassword3 "
+                                                            class="col-sm-2 col-form-label ">Jenis Mitra</label>
+                                                        <div class="col-sm-10 ">
+                                                            <input type="text" name="juduljenismitra"
+                                                                id="juduljenismitra" class="form-control "
+                                                                placeholder="Masukan Jenis Mitra Baru"
+                                                                value="{{ $item->juduljenismitra }}">
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="modal-footer justify-content-between">
-                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                                <button type="submit" class="btn btn-primary" value="Save">Simpan</button>
-                                            </div>
+                                                <div class="modal-footer justify-content-between">
+                                                    <button type="button" class="btn btn-default"
+                                                        data-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-primary"
+                                                        value="Save">Simpan</button>
+                                                </div>
                                             </form>
                                         </div>
                                         <!-- /.modal-content -->
@@ -114,19 +123,20 @@
                     </button>
                 </div>
                 <form action="{{route('tambah_mitra')}}" method="post">
-                {!! csrf_field() !!}
-                <div class="modal-body">
-                    <div class="form-group row ">
-                        <label for="inputPassword3 " class="col-sm-2 col-form-label ">Jenis Mitra</label>
-                        <div class="col-sm-10 ">
-                            <input type="text" name="juduljenismitra" id="juduljenismitra" class="form-control " placeholder="Masukan Jenis Mitra Baru">
+                    {!! csrf_field() !!}
+                    <div class="modal-body">
+                        <div class="form-group row ">
+                            <label for="inputPassword3 " class="col-sm-2 col-form-label">Jenis Mitra</label>
+                            <div class="col-sm-10 ">
+                                <input type="text" name="juduljenismitra" id="juduljenismitra" class="form-control "
+                                    placeholder="Masukan Jenis Mitra Baru">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary" value="Save">Tambah</button>
-                </div>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary" value="Save">Tambah</button>
+                    </div>
                 </form>
             </div>
             <!-- /.modal-content -->
@@ -135,7 +145,7 @@
     </div>
     <!-- /.modal -->
 
-    
+
 </section>
 
 @endsection
