9f338199b95d6d35c525b757db8705f5eba81b6f
diaaand <diaaanaaad@gmail.com>
2022-10-19 11:57:18 +0700

    Update base db baru kerjasama

---
+++ app/Http/Controllers/KerjasamaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kerjasama;
+use Illuminate\Http\Request;
+
+class KerjasamaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kerjasama  $kerjasama
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kerjasama $kerjasama)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kerjasama  $kerjasama
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kerjasama $kerjasama)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kerjasama  $kerjasama
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kerjasama $kerjasama)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kerjasama  $kerjasama
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kerjasama $kerjasama)
+    {
+        //
+    }
+}
