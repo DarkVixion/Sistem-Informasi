cb07f5b9799d829b9d7018f0f911d2cffaea39e4
Aran27 <ashabrivanyayardan@gmail.com>
2022-11-01 12:09:18 +0700

    Update

--- resources/views/AdminTemplate.blade.php
+++ resources/views/AdminTemplate.blade.php
@@ -110,7 +110,7 @@
                     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                         data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+                with font-awesome or any other icon font library -->
 
                         <li class="nav-item">
                             <a href="/AdminDashboard" class="nav-link">
